261397912144d0bb987773ebae462f74b163120e
botamochi6277 <botamochi6277@icloud.com>
2023-09-30 20:49:31 +0900

    add fetch_prototype

---
+++ scripts/fetch_prototype.js
@@ -0,0 +1,93 @@
+// node  --loader ts-node/esm scripts/fetch_prototype.ts
+import yargs from 'yargs';
+import { hideBin } from "yargs/helpers";
+import axios from 'axios';
+import fs from 'fs';
+
+const args = yargs(hideBin(process.argv))
+  .command("* <user_id> <api_key>", "fetch prototype data from ProtoPedia")
+  .positional("user_id", {
+    describe: "user_id to extract",
+    type: "string",
+    demandOption: true,
+  })
+  .positional("api_key", {
+    describe: "api_key to fetch prototype data",
+    type: "string",
+    demandOption: true,
+  })
+  .option("output", {
+    default: "protopytes.json",
+    type: "string"
+  })
+  .parseSync()
+
+const parseResponse = (data/* : PrototypeRawData[]*/) => {
+  if (data.length == 0) {
+    return;
+  }
+
+  // unique prototype_ids
+  const prototype_ids = data.map(d => d.id);
+  const prototype_uniq_ids = Array.from(new Set(prototype_ids));
+  console.log(`#prototypes ${prototype_uniq_ids.length} (${prototype_ids.length})`);
+  const idx = prototype_uniq_ids.map((id) => prototype_ids.findIndex(i => i == id));
+
+  return idx.map(i => data[i]);// prototypes
+}
+
+const parseProjectData = (data0 /* : PrototypeRawData*/) => {
+  if (!data0) {
+    return;
+  }
+  const images_ref = [
+    data0?.image1, data0?.image2, data0?.image3, data0?.image4, data0?.image5
+  ]
+  // material list with removing overlap values
+  // const mat = data.map(d => d.materialNm).filter((elem, index, self) => self.indexOf(elem) === index);
+  const images = images_ref.filter((im) => im); // remove null
+  const proto/*: PrototypeData*/ = {
+    name: data0.prototypeNm,
+    developing_status: data0.status,
+    images: images,
+    summary: data0.summary,
+    developer: data0.userNm,
+    team: data0.teamNm,
+    materials: data0.materialNm,
+    tags: data0.tags?.split(','),
+    prototype_id: data0.id
+  }
+  // console.log(proto);
+  return proto;
+}
+
+const fetchProjectData = (token/*: string*/, username/*: string*/, output) => {
+  // input validation
+  const token_ref = token.match(/[a-f0-9]{32}/g);
+  if (!token_ref) {
+    console.error('token must be 32 letters');
+    console.error(`your token is ${token}`)
+    return;
+  }
+
+  const url = `https://protopedia.net/api/prototypes.json?token=${token}&userNm=${username}`
+  axios.get(url).then((res) => {
+    // console.debug(res);
+    // protopedia APIの変な挙動
+    // 登録されているmaterial+eventの数だけprototype_raw dataが返ってくる...ことがある
+    // なんで materialsとかでまとめて返さないのだろうか？？
+    const data = parseResponse(res.data);
+    if (data) {
+      const prototypes = data.map(d => parseProjectData(d));
+      const datetime = Date();
+      const s = JSON.stringify({ datetime, prototypes });
+      fs.writeFileSync(output, s);
+
+    }
+  }).catch((error) => {
+    console.log(error);
+  })
+}
+
+fetchProjectData(args.api_key, args.user_id, args.output);
+
