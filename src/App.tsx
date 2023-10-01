3e7334ef4a3d341815f35636909b015cad8ccab3
botamochi6277 <botamochi6277@icloud.com>
2023-10-01 14:01:33 +0900

    add Protopedia Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import * as React from 'react';
+import {
+  Container,
+  Button,
+  createTheme, ThemeProvider, CssBaseline
+} from '@mui/material';
+
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
 import ProtoPediaCarousel from './ProtoPediaCarousel'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = React.useState(
+    createTheme({
+      palette: {
+        mode: 'dark',
+      },
+    })
+  );
+  const toggleTheme = (theme: any) => {
+    if (theme.palette.mode === 'dark') {
+      setTheme(createTheme({ palette: { mode: 'light', }, }))
+    } else {
+      setTheme(createTheme({ palette: { mode: 'dark', }, }))
+    }
+  }
+
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <ProtoPediaCarousel />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <h1>BotaLab</h1>
+        <h2>ProtoPedia Works</h2>
+        <ProtoPediaCarousel />
 
-      {/* Zenn */}
-      {/* Twitter */}
-      {/* GrabCad */}
-      {/* Thingiverse */}
-      {/* Vrm Hub */}
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        {/* Zenn */}
+        {/* Twitter */}
+        {/* GrabCad */}
+        {/* Thingiverse */}
+        {/* Vrm Hub */}
+        <Button
+          startIcon={theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+          variant="outlined"
+          color="secondary"
+          onClick={() => toggleTheme(theme)}
+          fullWidth
+        >
+          {theme.palette.mode} mode
+        </Button>
+      </Container>
+    </ThemeProvider>
   )
 }
 
