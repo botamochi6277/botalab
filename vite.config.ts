e1458c0794b8ced0fb5215c355578ed76d98043a
botamochi6277 <botamochi6277@icloud.com>
2023-10-01 20:04:06 +0900

    add ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://qiita.com/tat_mae084/items/4051c61926dc8165e80b
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "botalab"
+    : "./",
   plugins: [react()],
 })
