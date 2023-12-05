ff7ef1dd2a49c2a8c28d38f55e3b95c5d2a42d97
botamochi6277 <botamochi6277@icloud.com>
2023-12-05 21:06:34 +0900

    add CircleHead

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {
   Container,
   Button,
+  Box,
+  CardMedia,
   createTheme, ThemeProvider, CssBaseline, Typography
 } from '@mui/material';
 
@@ -10,6 +12,8 @@ import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 import ProtoPediaCarousel from './ProtoPediaCarousel'
 import MyAppBar from './MyAppBar';
+import CircleHead from './CircleHead';
+import ExhibitionTimeline from './ExhibitionTimeline';
 
 function App() {
   const [theme, setTheme] = React.useState(
@@ -27,19 +31,30 @@ function App() {
     }
   }
 
-
+  // try to create youtube channel top page
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container maxWidth="xl">
+      <Container maxWidth="lg">
         <MyAppBar />
+        {/* Banner/Header Image */}
+        {/* https://jp.cyberlink.com/blog/photoeditor/1755/best-photo-software-to-make-youtube-banners#:~:text=YouTube%20ヘッダー・バナーサイズと作成時の注意点,-ヘッダー・バナー作成&text=以下の図のよう,のサイズで作ります%E3%80%82 */}
+        <CardMedia component="img" image='https://dummyimage.com/2560x263/2b2b2b/f7f7f7&text=dummy_banner'>
+        </CardMedia>
+
+        <CircleHead title='BotaLab' subtitle='@botamochi6277' img='https://dummyimage.com/256x256/2b2bff/f7f7f7&text=B'
+          description={"趣味でメイカーとして活動しているbotamochiです. イベントでは個人サークルのBotaLabとして出展しています."} />
+
         <h2>About</h2>
         <Typography>
           エンジニア/メイカーのbotamochi6277が作品を紹介するページです.<br />
           botamochi6277, a engineer and a maker, introduces his works in this site.
         </Typography>
+        {/* https://mui.com/material-ui/react-tabs/ */}
         <h2>ProtoPedia Works</h2>
         <ProtoPediaCarousel />
+
+        <ExhibitionTimeline />
 
         <Button
           startIcon={theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
---
+++ src/CircleHead.tsx
@@ -0,0 +1,72 @@
+// header like youtube channel
+
+import * as React from 'react';
+import {
+  Typography, Link, IconButton,
+  Card,
+  CardContent,
+  CardActions,
+  Avatar,
+} from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+
+// icons
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ArchitectureIcon from '@mui/icons-material/Architecture';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import ArticleIcon from '@mui/icons-material/Article';
+import ViewInArIcon from '@mui/icons-material/ViewInAr';
+import HexagonIcon from '@mui/icons-material/Hexagon';
+
+export default function CircleHead(props: { title: string, subtitle: string, img: string, description: string | undefined }) {
+  const icon_items = [
+    { name: "grabcad", to: "https://grabcad.com/botamochi-1", icon: <ViewInArIcon /> },
+    { name: "thingiverse", to: "https://www.thingiverse.com/botamochi/designs", icon: <HexagonIcon /> },
+    { name: "zenn", to: "https://zenn.dev/botamochi6277", icon: <ArticleIcon /> },
+    { name: "twitter", to: "https://twitter.com/botamochi6277", icon: <TwitterIcon /> },
+    { name: "github", to: 'https://github.com/botamochi6277', icon: <GitHubIcon /> },
+  ]
+  return (
+    <Card >
+      <Grid container spacing={2}>
+        <Grid xs={4}>
+          <Avatar
+            alt="Remy Sharp"
+            src={props.img}
+            sx={{ width: 200, height: 200 }}
+          />
+        </Grid>
+        <Grid xs={8}>
+          <CardContent>
+            <Typography component="div" variant="h3">
+              {props.title}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              {props.subtitle}
+            </Typography>
+            {/* badges */}
+            <Typography>
+              {props.description}
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+            {/* media including twitter, zenn, github */}
+            {icon_items.map(item => (
+              <IconButton
+                component={Link}
+                href={item.to}
+                target="_blank"
+                rel="noopener"
+                sx={{ color: 'inherit' }}
+                key={item.name}
+              >
+                {item.icon}
+              </IconButton>
+            ))}
+          </CardActions>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+}
