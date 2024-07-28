c1e9dca66d8ec43eeb2f5c8b2df3447fafc39f6a
botamochi6277 <botamochi6277@icloud.com>
2024-07-28 17:42:43 +0900

    Add event logos

--- src/ExhibitionTimeline.tsx
+++ src/ExhibitionTimeline.tsx
@@ -1,65 +1,73 @@
-import Timeline from '@mui/lab/Timeline';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import { Icon, Link, Typography } from '@mui/material';
+import Timeline from "@mui/lab/Timeline";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import { Icon, Link, Typography, Avatar } from "@mui/material";
 
-
-import exhibitions from './assets/exhibitions.json';
+import exhibitions from "./assets/exhibitions.json";
 
 export default function ExhibitionTimeline() {
-
-  const items = exhibitions.exhibitions
+  const items = exhibitions.exhibitions;
   // sort
   items.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
   const exhibitionName = (name: string, link: string) => {
     if (link.length > 0) {
-      return (<Link href={link} target="_blank" underline="hover">
-        <Typography variant="h6" component="span">
-          {name}
-        </Typography >
-      </Link >)
+      return (
+        <Link href={link} target="_blank" underline="hover">
+          <Typography variant="h6" component="span">
+            {name}
+          </Typography>
+        </Link>
+      );
     }
 
     return (
       <Typography variant="h6" component="span">
         {name}
       </Typography>
-    )
-  }
+    );
+  };
 
   // console.log()
 
-  const tl_items = items.reverse().map(item => (
+  const tl_items = items.reverse().map((item) => (
     <TimelineItem key={item.name}>
       <TimelineOppositeContent
-        sx={{ m: 'auto 0', display: { xs: "none", sm: "block" } }}
+        sx={{ m: "auto 0", display: { xs: "none", sm: "block" } }}
         align="right"
         variant="body2"
         color="text.secondary"
       >
-        {item.date}{item?.period}
+        {item.date}
+        {item?.period}
       </TimelineOppositeContent>
       <TimelineSeparator>
         <TimelineConnector />
-        <TimelineDot color={((new Date(item.date).getTime() - new Date().getTime()) > 0) ? "success" : "primary"}>
-          <Icon>{item.icon}</Icon>
+        <TimelineDot
+          color={
+            new Date(item.date).getTime() - new Date().getTime() > 0
+              ? "success"
+              : "primary"
+          }
+        >
+          {item.icon.includes(".png") || item.icon.includes(".svg") ? (
+            <Avatar src={item.icon} sx={{ width: 24, height: 24 }} />
+          ) : (
+            <Icon>{item.icon}</Icon>
+          )}
         </TimelineDot>
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent sx={{ py: '12px', px: 2 }}>
+      <TimelineContent sx={{ py: "12px", px: 2 }}>
         {exhibitionName(item.name, item.link)}
         <Typography>{item.location}</Typography>
       </TimelineContent>
-    </TimelineItem>))
+    </TimelineItem>
+  ));
 
-  return (
-    <Timeline>
-      {tl_items}
-    </Timeline>
-  );
+  return <Timeline>{tl_items}</Timeline>;
 }
